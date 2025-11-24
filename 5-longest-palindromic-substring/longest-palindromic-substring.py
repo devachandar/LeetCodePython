9c40ca171b6cbf69b0145bd770a0fe679f1845ca
devachandar <47187469+devachandar@users.noreply.github.com>
2025-11-24 10:22:20 -0500

    Time: 263 ms (70.99%) | Memory: 18.1 MB (25.98%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/longest-palindromic-substring.py
@@ -1,29 +1,28 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        if not s or len(s) == 1:
-            return s
+        start = 0
+        end = 0
+        max_len = 0 
+        def expand(left:int,right:int):
 
-        start, end = 0, 0  # indices of best palindrome
-
-        def expand_from_center(left: int, right: int) -> (int, int):
-            # expand as long as it's a valid palindrome
             while left >= 0 and right < len(s) and s[left] == s[right]:
-                left -= 1
-                right += 1
-            # when the while loop stops, left and right are one step beyond
-            # the last valid palindrome, so actual palindrome is (left+1, right-1)
-            return left + 1, right - 1
+                left -=1
+                right +=1
+            
+            return left+1, right-1 
 
         for i in range(len(s)):
-            # Odd-length center at i
-            l1, r1 = expand_from_center(i, i)
-            # Even-length center between i and i+1
-            l2, r2 = expand_from_center(i, i + 1)
-
-            # Pick the longer of the two
-            if r1 - l1 > end - start:
-                start, end = l1, r1
-            if r2 - l2 > end - start:
-                start, end = l2, r2
-
-        return s[start:end + 1]
+            l1, r1 = expand(i,i)
+            l2, r2 = expand(i,i+1)
+            
+            if r1-l1+1 > max_len:
+                max_len =r1-l1+1
+                start = l1
+                end = r1
+            if r2-l2+1 > max_len:
+                max_len = r2-l2+1
+                print("max",max_len,r2,l2)
+                start = l2
+                end = r2
+        
+        return s[start:end+1]
