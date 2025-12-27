d7bcf89d3dc5093e0c99e5893e9a323ba76cbcbe
devachandar <47187469+devachandar@users.noreply.github.com>
2025-12-27 12:08:20 -0500

    Time: 8 ms (28.29%) | Memory: 24.3 MB (95.51%) - LeetSync

--- 189-rotate-array/rotate-array.py
+++ 189-rotate-array/rotate-array.py
@@ -3,12 +3,33 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        rotated_nums = []
-        if k > len(nums):
-            k = k % len(nums)
-        for i in range(len(nums)-k,len(nums)):
-            rotated_nums.append(nums[i])
-        for i in range(len(nums)-k):
-            rotated_nums.append(nums[i])
-        nums[:] = rotated_nums
+        n = len(nums)
+        k = k % n   # important!
+
+        def reverse(left, right):
+            while left < right:
+                nums[left], nums[right] = nums[right], nums[left]
+                left += 1
+                right -= 1
+
+        # Step 1: reverse entire array
+        reverse(0, n - 1)
+
+        # Step 2: reverse first k elements
+        reverse(0, k - 1)
+
+        # Step 3: reverse remaining elements
+        reverse(k, n - 1)
+
+
+
+        # Using extra variable
+        # rotated_nums = []
+        # if k > len(nums):
+        #     k = k % len(nums)
+        # for i in range(len(nums)-k,len(nums)):
+        #     rotated_nums.append(nums[i])
+        # for i in range(len(nums)-k):
+        #     rotated_nums.append(nums[i])
+        # nums[:] = rotated_nums
         
