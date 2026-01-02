4b3c5f87fd705d0c8f9bc71a24e56331fc367049
devachandar <47187469+devachandar@users.noreply.github.com>
2026-01-02 17:04:46 -0500

    Time: 0 ms (100.00%) | Memory: 17.4 MB (86.34%) - LeetSync

--- 392-is-subsequence/is-subsequence.py
+++ 392-is-subsequence/is-subsequence.py
@@ -4,7 +4,6 @@ class Solution:
             return True
         j = 0
         for i in range(len(t)):
-            
             if t[i] == s[j]:
                 j+=1
                 if j >= len(s):
