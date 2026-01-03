623c3224f75f6c2c87636f71037eb5c80a7171de
devachandar <47187469+devachandar@users.noreply.github.com>
2026-01-02 20:33:47 -0500

    Time: 7 ms (83.00%) | Memory: 17.7 MB (88.26%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,19 +1,36 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
 
-        left = 0 
-        right = len(s) - 1
+
+        left, right = 0, len(s) - 1
         s = s.lower()
-        while left<right:
-            if not (('0' <= s[left] <= '9') or ('a' <= s[left] <= 'z')):
-                left +=1
-            elif not (('0' <= s[right] <= '9') or ('a' <= s[right] <= 'z')):
-                right -=1
+
+        while left < right:
+            if not s[left].isalnum():
+                left += 1
+            elif not s[right].isalnum():
+                right -= 1
             elif s[left] != s[right]:
-                print(s[left],s[right],left,right)
                 return False
             else:
-                left +=1
-                right -=1
-        
+                left += 1
+                right -= 1
+
         return True
+        
+        # left = 0 
+        # right = len(s) - 1
+        # s = s.lower()
+        # while left<right:
+        #     if not (('0' <= s[left] <= '9') or ('a' <= s[left] <= 'z')):
+        #         left +=1
+        #     elif not (('0' <= s[right] <= '9') or ('a' <= s[right] <= 'z')):
+        #         right -=1
+        #     elif s[left] != s[right]:
+        #         print(s[left],s[right],left,right)
+        #         return False
+        #     else:
+        #         left +=1
+        #         right -=1
+        
+        # return True
