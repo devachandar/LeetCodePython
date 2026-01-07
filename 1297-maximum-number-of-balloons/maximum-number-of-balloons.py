d6b9663dc929f90e0f697a000a3d315c5a888306
devachandar <47187469+devachandar@users.noreply.github.com>
2026-01-07 10:37:16 -0500

    Time: 3 ms (60.49%) | Memory: 19.2 MB (18.37%) - LeetSync

--- 1297-maximum-number-of-balloons/maximum-number-of-balloons.py
+++ 1297-maximum-number-of-balloons/maximum-number-of-balloons.py
@@ -1,12 +1,21 @@
 class Solution:
     def maxNumberOfBalloons(self, text: str) -> int:
-        freq = Counter(text)
+       
+        freq = {'b':0,'a':0,'l':0,"o":0,"n":0}
+        for i in text:
+            if i not in freq:
+                freq[i] = 1
+            else:
+                freq[i]+=1
+        return min(freq['b'],freq['a'],int(freq['l'] / 2),int(freq['o'] / 2),freq['n'])
 
-        print(freq)
-        # freq = {'b':0,'a':0,'l':0,"o":0,"n":0}
+
+
+        # freq = Counter(text)
+        # freq = 'b':0,'a':0,'l':0,"o":0,"n":0}
         # for i in text:
         #     if i not in freq:
         #         freq[i] = 1
         #     else:
         #         freq[i]+=1
-        return min(freq['b'],freq['a'],int(freq['l'] / 2),int(freq['o'] / 2),freq['n'])
+        # return min(freq['b'],freq['a'],int(freq['l'] / 2),int(freq['o'] / 2),freq['n'])
