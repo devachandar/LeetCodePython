62ff3cef2264e8534a1c65e2756cef48cd7ed75b
devachandar <47187469+devachandar@users.noreply.github.com>
2026-01-11 11:25:06 -0500

    Time: 50 ms (56.01%) | Memory: 36.6 MB (17.44%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.py
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.py
@@ -1,39 +1,32 @@
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
 
-        num_set = set(nums)
-        longest = 0
+        # num_set = set(nums)
+        # longest = 0
 
-        for num in num_set:
-            # only start counting if num is the start of a sequence
-            if num - 1 not in num_set:
-                current = num
-                length = 1
+        # for num in num_set:
+        #     # only start counting if num is the start of a sequence
+        #     if num - 1 not in num_set:
+        #         current = num
+        #         length = 1
 
-                while current + 1 in num_set:
-                    current += 1
-                    length += 1
+        #         while current + 1 in num_set:
+        #             current += 1
+        #             length += 1
 
-                longest = max(longest, length)
+        #         longest = max(longest, length)
 
-        return longest
-        
-        # nums.sort()
-        # visited = []
-        # max_count = 0
-        # for num in nums:
-        #     if num not in visited:
-        #         visited.append(num)
-        #         temp = num+1
-        #         count = 1
-        #         # print(num, temp)
-        #         while temp in nums:
-        #             visited.append(temp)
-        #             count+=1
-        #             temp+=1
-        #             # else:
-        #             #     break
-        #         max_count = max(max_count,count)
+        # return longest
+        nums = set(nums)
+        max_count = 0
+        for num in nums:
+            if num-1 not in nums:
+                temp = num+1
+                count = 1
+                while temp in nums:
+                    count+=1
+                    temp+=1
+                max_count = max(max_count,count)
         
         return max_count
 
