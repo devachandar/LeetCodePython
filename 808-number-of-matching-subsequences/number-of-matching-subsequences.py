d93e629326255a4c9d06af2f4f4b7d61d7434cd1
devachandar <47187469+devachandar@users.noreply.github.com>
2026-01-11 18:30:18 -0500

    Time: 1190 ms (11.66%) | Memory: 20.8 MB (41.20%) - LeetSync

--- 808-number-of-matching-subsequences/number-of-matching-subsequences.py
+++ 808-number-of-matching-subsequences/number-of-matching-subsequences.py
@@ -1,5 +1,8 @@
 class Solution:
     def numMatchingSubseq(self, s: str, words: List[str]) -> int:
+
+        
+
         count = 0
         def is_sequence(word):
             temp = s
@@ -16,15 +19,3 @@ class Solution:
                 count+=1
         
         return count
-
-        # count = 0
-        # s_freq = Counter(s)
-        # for word in words:
-        #     word_freq = Counter(word)
-        #     for key in word_freq:
-        #         if s_freq[key] < word_freq[key]:
-        #             break      
-        #     else:  
-        #         count+=1
-        
-        # return count
