209004671b443398c9db00cc6b0118a6925d2fbd
devachandar <47187469+devachandar@users.noreply.github.com>
2026-05-15 13:11:34 -0400

    Time: 83 ms (5.25%) | Memory: 20.8 MB (16.65%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
@@ -15,10 +15,10 @@ class Solution:
             
         return len(nums)
 
-        count = 1
+        # count = 1
 
-        for i in range(len(nums)-1):
-            if nums[i] != nums[i+1]:
-                count +=1
+        # for i in range(len(nums)-1):
+        #     if nums[i] != nums[i+1]:
+        #         count +=1
             
-        return count
+        # return count
