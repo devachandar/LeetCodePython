fcacecb045c9f64963794cd461d6c222f8afa84d
devachandar <47187469+devachandar@users.noreply.github.com>
2026-06-02 09:19:45 -0400

    Time: 4 ms (94.57%) | Memory: 19.2 MB (79.74%) - LeetSync

--- 6-zigzag-conversion/zigzag-conversion.py
+++ 6-zigzag-conversion/zigzag-conversion.py
@@ -1,68 +1,25 @@
 class Solution:
     def convert(self, s: str, numRows: int) -> str:
+    
         if numRows == 1:
-            return s
+            return s 
+        rows = [""] * numRows
+        currRow = 0
+        goingDown= False
 
-        ptr = 0
-        result_list = [""] * numRows
-        print(result_list)
-        gng_down = False
-        # gng_up = False
-
-        for ch in s:
-            if ptr == 0:
-                gng_down = True
+        for letter in s:
+            if currRow == 0:
+                goingDown = True
             
-            if ptr == numRows-1:
-                gng_down = False
+            if currRow == numRows - 1:
+                goingDown = False
+            
+            
+            rows[currRow]+= letter
 
-            result_list[ptr]+= ch
-
-            if gng_down:
-                ptr+=1
+            if goingDown == True:
+                currRow +=1
             else:
-                ptr-=1
-            
-        return "".join(result_list)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        # if numRows == 1:
-        #     return s 
-        # rows = [""] * numRows
-        # currRow = 0
-        # goingDown= False
-
-        # for letter in s:
-        #     if currRow == 0:
-        #         goingDown = True
-            
-        #     if currRow == numRows - 1:
-        #         goingDown = False
-            
-            
-        #     rows[currRow]+= letter
-        #     print("dd", rows)
-
-        #     if goingDown == True:
-        #         currRow +=1
-        #     else:
-        #         currRow -=1
+                currRow -=1
         
-        # return "".join(rows)
+        return "".join(rows)
