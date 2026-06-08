81b0145b6f4e621d182678540564799e24590c37
devachandar <47187469+devachandar@users.noreply.github.com>
2026-06-08 09:40:31 -0400

    Time: 50 ms (23.02%) | Memory: 19.4 MB (17.58%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.py
+++ 1894-merge-strings-alternately/merge-strings-alternately.py
@@ -13,19 +13,3 @@ class Solution:
         
         return new_str
 
-
-        # n = len(word1)
-        # m = len(word2)
-        # left = 0
-        # right = 0
-        # new_str = ""
-        # while left < n or right < m:
-        #     if left < n :
-        #         new_str += word1[left]
-        #         left +=1
-        #     if right < m:
-        #         new_str += word2[right]
-        #         right +=1
-
-
-        # return new_str 
